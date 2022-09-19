141ec41737a38827c635b57dc4b8ca1bc16b7baa
Ciprian Draghici <ciprian@shoreline.io>
2022-09-19 19:29:43 +0300

    add logs into addTagToJiraIssues

--- dist/index.js
+++ dist/index.js
@@ -20693,17 +20693,35 @@ function makeTags(tag, prefix, additionalTag) {
 }
 function addTagToJiraIssuesBuilder(generateChangelogService, jiraClient, info) {
     return async ({ rawTag, tagFieldName, prefix, additionalTag, }) => {
+        // eslint-disable-next-line no-console
+        console.log('Enter to AddTagToJiraIssues - rawTag = ', rawTag);
         const tag = new Tag(rawTag);
+        // eslint-disable-next-line no-console
+        console.log('tag = ', tag);
         const changelog = await generateChangelogService({ rawHeadTag: tag.value });
+        // eslint-disable-next-line no-console
+        console.log('changelog = ', changelog);
         const issuesKeys = changelog
             .filter((item) => item.existsInJira)
             .map((item) => item.issueKey)
             .filter((item) => item != null);
+        // eslint-disable-next-line no-console
+        console.log('issuesKeys = ', issuesKeys);
         const issues = await jiraClient.getIssuesByKeys(issuesKeys);
+        // eslint-disable-next-line no-console
+        console.log('issuesKeys = ', issuesKeys);
         const foundIssueKeys = issues.map((i) => i.key);
+        // eslint-disable-next-line no-console
+        console.log('foundIssueKeys = ', foundIssueKeys);
         const fields = await jiraClient.getCustomFields();
+        // eslint-disable-next-line no-console
+        console.log('fields = ', JSON.stringify(fields));
         const tagField = fields.find((field) => field.name.toLowerCase() === tagFieldName.toLowerCase());
+        // eslint-disable-next-line no-console
+        console.log('tagField = ', JSON.stringify(tagField));
         if (tagField == null) {
+            // eslint-disable-next-line no-console
+            console.log('tagField = null');
             return {
                 updatedIssues: [],
                 allIssues: foundIssueKeys,
@@ -20713,9 +20731,13 @@ function addTagToJiraIssuesBuilder(generateChangelogService, jiraClient, info) {
         await Promise.all(issues.map(async (issue) => {
             const prevTags = issue.fields[tagField.id] ?? [];
             if (!checkIsStringArray(prevTags)) {
+                // eslint-disable-next-line no-console
+                console.log('!checkIsStringArray(prevTags) = ', JSON.stringify(issue));
                 return;
             }
             try {
+                // eslint-disable-next-line no-console
+                console.log('try to update issue - tagField.id = ', tagField.id);
                 await jiraClient.updateIssue({
                     fields: {
                         [tagField.id]: unique([
@@ -20724,10 +20746,16 @@ function addTagToJiraIssuesBuilder(generateChangelogService, jiraClient, info) {
                         ]),
                     },
                 }, issue.key);
+                // eslint-disable-next-line no-console
+                console.log('updated issue in JIRA - issue id = ', issue.id);
                 updatedIssues.push(issue.key);
             }
             catch (e) {
+                // eslint-disable-next-line no-console
+                console.log('updated issue Error');
                 if (e instanceof Error) {
+                    // eslint-disable-next-line no-console
+                    console.log('Error Message = ', e.toString());
                     info(e.toString());
                 }
             }
